c768b465de2d1cf02020f2ba813c75e15de876b7
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-02-28 21:36:52 +0200

    add: contact us form

---
+++ react-frontend/src/components/ContactUs/contactus.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './contactus.css';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+
+
+const theme = createTheme();
+
+export default function Contactus() {
+    
+    const [ redirect, setRedirect ] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+    };
+    
+    return (
+    <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+        >
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    autoFocus
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextareaAutosize
+                    aria-label="empty textarea"
+                    placeholder="Comment"
+                    maxRows={5}
+                    style={{ width: 400 }}
+                />
+                </Grid>
+            </Grid>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Submit
+            </Button>
+            </Box>
+        </Box>
+        </Container>
+    </ThemeProvider>
+    );
+}
--- react-frontend/src/pages/contactUs.js
+++ react-frontend/src/pages/contactUs.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Contactus from '../components/ContactUs/contactus';
 
 export default function ContactUs() {
   return (
-    <h2>Contact Us</h2>
+      <section className="contact-us">
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1 className='text-success color'>Contact Us!</h1>
+                        <h2>Please Fill Out This Form</h2>
+                        <hr/>
+                        <Contactus/>
+                    </div>
+                </div>
+            </div>
+        </section>
   );
 }
