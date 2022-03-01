c5c4b0c26f9b6f2558dc2a9d5e028dd76ce52a78
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-01 07:49:22 +0200

    change: navbar bg color

--- react-frontend/src/components/NavBar/Navbar.js
+++ react-frontend/src/components/NavBar/Navbar.js
@@ -55,7 +55,7 @@ export default function Navbar(props) {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
